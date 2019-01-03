85e809497bec29545aed517f6404ed8a5de8ff69
Gene83 <gene83@protonmail.com>
2019-01-02 18:36:06 -1000

    tests passing

--- vault.js
+++ vault.js
@@ -1,4 +1,20 @@
-'use strict';
+"use strict";
 module.exports = function() {
-  
+  const vault = {};
+
+  function getValue(key) {
+    if (key === undefined || !(key in vault)) {
+      return null;
+    }
+    return vault[key];
+  }
+
+  function setValue(key, value) {
+    vault[key] = value;
+  }
+
+  return {
+    getValue,
+    setValue
+  };
 };
